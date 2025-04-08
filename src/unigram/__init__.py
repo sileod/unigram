8b7942b2dff26e7885d3e2f70e9bb500f68354e2
Damien Sileo <dsileo@magnet9.lille.inria.fr>
2025-04-08 15:01:41 +0200

    added test

--- src/unigram/__init__.py
+++ src/unigram/__init__.py
@@ -358,11 +358,10 @@ def generate(start, n_iter=10_000, mode='recursive', *args, **kwargs):
     raise ValueError('Incomplete generation')
 
 
-    
+
 def G0():
     """Toy grammar for illustration"""
-    R=init_grammar(['tptp','eng'])
-    
+    R = init_grammar(['tptp','eng'])
     def render_branch(x):
         #print(x.root, x.root.leaves, x.root.descendants, x.leftsbling)
         """equivalent to '0', illustrate function-based rendering"""
@@ -381,45 +380,45 @@ def G0():
     R('op', '|', 'or')
     return R
 
-<<<<<<< Updated upstream
-def LogicNLI():    
+
+def LogicNLI():
     ADJECTIVES = ['rich', 'quiet', 'old', 'tall', 'kind', 'brave', 'wise',
                   'happy', 'strong', 'curious', 'patient', 'funny', 'generous', 'humble']
     # (We selected adjectives with no clear semantic interference)
     NAMES = ['mary', 'paul', 'fred', 'alice', 'john', 'susan', 'lucy']
-    
-    R.init(['tptp', 'eng'], "fof")
+
+    R = init_grammar(['tptp','eng'])
     R('start(' + ','.join(['rule']*16) + ',' + ','.join(['fact']*8) + ')',
       '&\n'.join([f'({i})' for i in range(24)]),
       '\n'.join([f'{i}' for i in range(24)]))
-    
+
     R('hypothesis(person,a)', '1(0)', '0 is 1')
-    
     for a in ADJECTIVES:
         R('adj', a)
         R('adj', f'~{a}', f'not {a}', weight=0.2)
-    
+
     R('property(adj,adj)', '(0(?)&1(?))', 'both 0 and 1')
     R('property(adj,adj)', '(0(?)|1(?))', '0 or 1')
     R('property(adj,adj)', '(0(?)<~>1(?))', 'either 0 or 1', weight=0.5)
     R('property(adj)', '0(?)', '0')
-    
+
     R('rule(property,property)', '![X]:(0[?←X]=>1[?←X])',
       'everyone who is 0 is 1')
     R('rule(property,property)', '![X]:(0[?←X]<=>1[?←X])',
       'everyone who is 0 is 1 and vice versa')
-    
+
     for p in NAMES:
         R('person', p)
-    
+
     R('fact(person,property)', '1[?←0]', '0 is 1')
     R('fact(property)', '?[X]:(0[?←X])', 'someone is 0', weight=0.2)
     R('rule(fact,fact)', '(0)=>(1)', 'if 0 then 1')
     R('rule(fact,fact)', '(0)<=>(1)', 'if 0 then 1 and vice versa')
     return R
-=======
-def test_G0():
+
+
+def test_grammar(grammar=G0()):
     "Illustrate the generation process"
-    production = generate(G0())
+    production = generate(grammar)
     return production, production@"eng"
->>>>>>> Stashed changes
+
